58b53f25ee21739f45fab2035c825a0356da84ed
rahmor <rahmor@yahoo.com>
2019-06-17 12:44:30 -0400

    start ex5

---
+++ exercise5.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const HashMap = require('./hashMap');
+const string = 'acecarr';
+const palindrome = new HashMap();
+
+//push each letter in string to hashmap.
+// {'key': a,
+// value : 1
+// }
+
+for (let i = 0; i < string.length; i++) {
+  palindrome.set();
+}
+//create hashmap for each permutation for string.
+//check if any in hashmap are palindromes.
